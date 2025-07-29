eccd0d8ef559cc8566f4dbf18018efe8f9eb7dd7
Edward Palmer <{username}@users.noreply.github.com>
2025-07-29 22:06:02 +0100

    Fixed bugs in server

--- src/libs/Socket/Server.cpp
+++ src/libs/Socket/Server.cpp
@@ -39,6 +39,12 @@ Server::Server(Port port) : _port(port)
         throw std::runtime_error("failed to bind server socket to address");
     }
 
+    /* Start listening */
+    if (listen(_listeningSocket, SOMAXCONN) == (-1))
+    {
+        throw std::runtime_error("failed to listen on socket");
+    }
+
     /* Create thread for processing received messages */
     _receivedMessageLoopThread = std::thread(&Server::receivedMessageQueueLoop, this);
 
@@ -75,28 +81,34 @@ void Server::listenLoop()
     fds.fd = _listeningSocket;
     fds.events = POLLIN; /* POLLIN: data to read */
 
-    int clientSocket{-1};
-
     while (true)
     {
-        /* TODO: - handle connection close and kill thread */
-        if (poll(&fds, 1, 1000) != (-1)) /* 1000ms between polls */ // TODO: - fix this block of code
-        /* GOOD REFERENCE: https://www.ibm.com/docs/en/i/7.1.0?topic=designs-using-poll-instead-select */
-        {
-            if (fds.revents & POLLIN)
-            {
-                if ((clientSocket = accept(_listeningSocket, nullptr, nullptr)) != (-1))
-                {
-                    Logger::instance().log("received connection request from client on socket " + std::to_string(clientSocket));
+        int pollResult = poll(&fds, 1, 1000); /* 1000ms between polls */
 
-                    Logger::instance().log("accepted connection on client socket " + std::to_string(clientSocket), Logger::Info);
-                    std::thread(&Server::connectionLoop, this, clientSocket);
-                }
-            }
+        if (pollResult == 0)
+        {
+            /* System call timed out with no new connections */
+        }
+        else if (pollResult == (-1))
+        {
+            Logger::instance().log("polling error occurred", Logger::Error);
         }
         else
         {
-            Logger::instance().log("no connection request from client");
+            if (fds.revents & POLLIN) /* New incoming connection */
+            {
+                int clientSocket = accept(_listeningSocket, nullptr, nullptr);
+                if (clientSocket == (-1))
+                {
+                    Logger::instance().log("failed to accept connection from client", Logger::Error);
+                }
+
+                /* Create and detach a new thread to handle messages from the client */
+                Logger::instance().log("accepted connection from client on socket " + std::to_string(clientSocket));
+
+                auto thread = std::thread(&Server::connectionLoop, this, clientSocket);
+                thread.detach(); /* Behaves independently to server ?? */
+            }
         }
     }
 }
@@ -112,22 +124,36 @@ void Server::connectionLoop(SocketFD clientSocket)
         return;
     }
 
-    const unsigned int kMessageBufferSize = 2048;
-
-    auto messageBuffer = std::make_unique<char>(kMessageBufferSize);
+    char messageBuffer[2048];
 
     while (true)
     {
-        if ((recv(clientSocket, messageBuffer.get(), kMessageBufferSize - 1, 0)) != (-1)) /* Blocking call */
+        /* Wipe buffer */
+        memset(messageBuffer, 0, 2048);
+
+        long nBytesRead = recv(clientSocket, messageBuffer, 2047, 0);
+
+        if (nBytesRead == 0)
+        {
+            /* Orderly shutdown. We can close the connection loop */
+            Logger::instance().log("client connection has closed. Shutting down connection loop thread");
+            return;
+        }
+        else if (nBytesRead == (-1))
+        {
+            Logger::instance().log("can error occurred in recv() from client socket " + std::to_string(clientSocket));
+            /* Continue for now */
+        }
+        else
         {
             Logger::instance().log("received message from client socket " + std::to_string(clientSocket));
 
             {
                 std::unique_lock lock(_receivedMessageMutex);
-                _receivedMessageQueue.push(std::string(messageBuffer.get()));
+                _receivedMessageQueue.push(std::string(messageBuffer, nBytesRead));
             }
 
-            _conditionVariable.notify_one();
+            _conditionVariable.notify_one(); /* Notify the message queue loop to handle the received message */
         }
     }
 }
