d5417d462a1557c6468623a09fbe619dda106f4e
Edward Palmer <{username}@users.noreply.github.com>
2025-08-16 10:30:53 +0100

    feat: adds startup.sh script

---
+++ scripts/startup.sh
@@ -0,0 +1,32 @@
+# !/user/bin/bash
+#
+# Author: Edward Palmer
+# Date: 2025-08-16
+# Copyright (c) 2025
+#
+
+EXCHANGE_PORT=1234
+DATABASE_PORT=2345
+ENGINE_PORT=3456
+
+echo "Commencing application startup..."
+
+LOGDIR="/var/log/talos"
+mkdir -p $LOGDIR
+
+# Start the dummy exchange:
+bazel run //src/apps:exchange_app $EXCHANGE_PORT > "$LOGDIR/exchange.log" &
+sleep 0.5
+
+# Start the dummy database:
+bazel run //src/apps:database_app $DATABASE_PORT > "$LOGDIR/database.log" &
+sleep 0.5
+
+# Start the Talos OMEngine:
+bazel run //src/apps:engine_app -- --engine $ENGINE_PORT --exchange $EXCHANGE_PORT --database $DATABASE_PORT > "$LOGDIR/engine.log" &
+sleep 0.5
+
+# Start the dummy order generator client:
+bazel run //src/apps:order_generator_app $ENGINE_PORT > "$LOGDIR/ordergenerator.log" &
+
+echo "Completed application startup..."
